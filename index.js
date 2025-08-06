86c41262ae093b63b1c8b0c40e6aadd75cea95b5
Tr14 <xuantrucx111@gmail.com>
2025-08-06 10:44:13 +0700

    add public page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
+const path = require("path");
 
 const app = express();
 const PORT = 3005;
